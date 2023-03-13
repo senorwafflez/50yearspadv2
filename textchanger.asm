da49aa3eb0f29e75f2e2200754d6d7902441d84d
SenorWaffleZ <senorwafflez@protonmail.com>
2023-03-13 19:32:42 +0100

    Place text

--- textchanger.asm
+++ textchanger.asm
@@ -1,8 +1,8 @@
 .pc = $4200 "Textchanger"
 
 //.var charsetload = $4000
-.var lineadd = $140
-.var line1start = $2140
+.var lineadd = $280
+.var line1start = $2000 + $140 * 6
 
 textchanger:
     ldx #$00   
@@ -73,9 +73,7 @@ textchanger:
             iny
         }
     }
-//     rts
 
-// textchanger2:
     ldx #$00   
     .for (var j = 0; j < 16; j++)
     {       
@@ -98,6 +96,52 @@ textchanger:
             iny
         }
     }
+
+    ldx #$00   
+    .for (var j = 0; j < 16; j++)
+    {       
+        lda textforoutput + 16*4 + j,x
+        asl
+        asl
+        asl
+        sta $fa
+        lda #$40
+        sta $fb
+        bcc lowchar        
+        inc $fb
+
+        lowchar:
+        ldy #$00
+        .for (var i = 0; i < 8; i++)
+        {
+            lda ($fa),y
+            sta line1start + lineadd * 4 + j * 8,y
+            iny
+        }
+    }
+
+    ldx #$00   
+    .for (var j = 0; j < 16; j++)
+    {       
+        lda textforoutput + 16*5 + j,x
+        asl
+        asl
+        asl
+        sta $fa
+        lda #$40
+        sta $fb
+        bcc lowchar        
+        inc $fb
+
+        lowchar:
+        ldy #$00
+        .for (var i = 0; i < 8; i++)
+        {
+            lda ($fa),y
+            sta line1start + lineadd * 5 + j * 8,y
+            iny
+        }
+    }
     rts
 
 textforoutput:
@@ -107,4 +151,3 @@ textforoutput:
     .text "ghijklmnopqrstuv"
     .text "wxyzabcdefghijkl"
     .text "mnopqrstuvwxyz12"
-    .text "3456789012345678"
