d0c0c354f0af477e132412859e851c8419159648
SenorWaffleZ <senorwafflez@protonmail.com>
2023-03-18 16:38:29 +0100

    Change temp text

--- greetingstext.asm
+++ greetingstext.asm
@@ -1,48 +1,48 @@
 .pc = $0900 "Greetingstext"
 
 greetings:
-    .text "1234567890123456"
-    .text "abcdefghijklmnop"
-    .text "qrstuvwxyzabcdef"
-    .text "ghijklmnopqrstuv"
-    .text "wxyzabcdefghijkl"
-    .text "         -merlin"
+    .text "wafflez4u waffle"
+    .text "z4u wafflez4u wa"
+    .text "fflez4u wafflez4"
+    .text "u wafflez4u waff"
+    .text "lez4u wafflez4u "
+    .text "wafflez4u waffle"
     .fill $20, $00
 
 //block 2
-    .text "abcdefghijklmnop"
-    .text "1234567890123456"
-    .text "qrstuvwxyzabcdef"
-    .text "ghijklmnopqrstuv"
-    .text "wxyzabcdefghijkl"
-    .text "        -hedning"
+    .text "pancakez4u panca"
+    .text "akez4u pancakez4"
+    .text "u pancakez4u pan"
+    .text "cakez4u pancakez"
+    .text "4u pancakez4u pa"
+    .text "ncakez4u pancake"
     .fill $20, $00
 
 //block 3
-    .text "mnopqrstuvwxyz12"
-    .text "1234567890123456"
-    .text "abcdefghijklmnop"
-    .text "qrstuvwxyzabcdef"
-    .text "ghijklmnopqrstuv"
-    .text "           -xiny"
+    .text "cookiez4u cookie"
+    .text "cookiez4u cookie"
+    .text "cookiez4u cookie"
+    .text "cookiez4u cookie"
+    .text "cookiez4u cookie"
+    .text "cookiez4u cookie"
     .fill $20, $00
 
 //etc
-    .text "ghijklmnopqrstuv"
-    .text "abcdefghijklmnop"
-    .text "qrstuvwxyzabcdef"
-    .text "mnopqrstuvwxyz12"
-    .text "wxyzabcdefghijkl"
-    .text "1234567890123456"
+    .text "balloonz4u ballo"
+    .text "balloonz4u ballo"
+    .text "balloonz4u ballo"
+    .text "balloonz4u ballo"
+    .text "balloonz4u ballo"
+    .text "balloonz4u ballo"
     .fill $20, $00
 
 //block 2
-    .text "vafler til deg x"
-    .text "abcdefghijklmnop"
-    .text "warp speed mr su"
-    .text "ghijklmnopqrstuv"
-    .text "wxyzabcdefghijkl"
-    .text "mnopqrstuvwxyz12"
+    .text "wafflewafflewaff"
+    .text "wafflewafflewaff"
+    .text "wafflewafflewaff"
+    .text "wafflewafflewaff"
+    .text "wafflewafflewaff"
+    .text "wafflewafflewaff"
     .fill $20, $00
 
 //block 3
